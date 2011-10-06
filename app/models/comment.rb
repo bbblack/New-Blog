349538d68d61f386771e8665eca43a26b30e7965
Hao Bai <bbsirius@ubuntu.(none)>
2011-10-06 09:52:32 -0600

    Initial commit

---
+++ app/models/comment.rb
@@ -0,0 +1,6 @@
+class Comment < ActiveRecord::Base
+	belongs_to :post
+	validates :post_id, :presence => true
+	validates :body, :presence => true
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '4a0d86e6ea52d904782799cfeaf93c405b84c14ed5c26892e134dfc63ef860cc1121413a1dd0b2e89a6634d0c8862e5c4acd79780abee5d3aebe3d83a1be6074'
